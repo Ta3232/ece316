c9021c3672b0e41f2ec28a48ca2be1d4311f9148
Ta3232 <84426477+Ta3232@users.noreply.github.com>
2026-01-19 02:49:18 -0500

    Update deliver.c

--- deliver.c
+++ deliver.c
@@ -8,10 +8,9 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#define BUF_SIZE 1024
 ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
 const struct sockaddr *dest_addr, socklen_t addrlen);
-
-#define BUF_SIZE 1024
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
