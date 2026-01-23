13dc329f5c06a9ef615495f22549fca2c626c840
Ta3232 <84426477+Ta3232@users.noreply.github.com>
2026-01-23 01:22:57 -0500

    Update deliver.c

--- deliver.c
+++ deliver.c
@@ -5,12 +5,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 
 #define BUF_SIZE 1024
-ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
-const struct sockaddr *dest_addr, socklen_t addrlen);
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -18,79 +14,94 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    char *server_ip = argv[1];
-    int port = atoi(argv[2]);
-    if (port <= 0 || port > 65535) {
+    // Parse port safely
+    char *end = NULL;
+    long port_long = strtol(argv[2], &end, 10);
+    if (!end || *end != '\0' || port_long <= 0 || port_long > 65535) {
         fprintf(stderr, "Invalid port number\n");
         return 1;
     }
 
-    //INPUT
+    // INPUT: expecting "ftp <filename>"
     char command[16];
     char filename[256];
-
     printf("ftp <file name>\n");
     if (scanf("%15s %255s", command, filename) != 2) {
         fprintf(stderr, "Invalid input format\n");
         return 1;
     }
+    if (strcmp(command, "ftp") != 0) {
+        fprintf(stderr, "Expected command: ftp <file name>\n");
+        return 1;
+    }
 
+    // Check file exists
     FILE *fp = fopen(filename, "rb");
     if (fp == NULL) {
-        //printf("don't exist\n");
+        // File doesn't exist => exit (per lab)
         return 1;
     }
     fclose(fp);
-    //printf("ftp\n");
 
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("socket");
-        return 1;
-    }
-
-    //get server addr
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons((uint16_t)port);
-
-    //inet_pton(AF_INET, server_ip, &server_addr.sin_addr);
-    struct addrinfo hints, *res = NULL;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
+    // Resolve server address (hostname OR IP), UDP
+    struct addrinfo hints, *servinfo = NULL, *p = NULL;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;      // IMPORTANT: allow IPv4 or IPv6 (hostnames may return either)
     hints.ai_socktype = SOCK_DGRAM;
 
-    int err = getaddrinfo(server_ip, argv[2], &hints, &res);
-    if (err != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(err));
-        close(sockfd);
+    int rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo);
+    if (rv != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    memcpy(&server_addr, res->ai_addr, sizeof(server_addr));
-    freeaddrinfo(res);
+    // Create a socket using the first usable result
+    int sockfd = -1;
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sockfd >= 0) break;
+    }
+    if (sockfd < 0) {
+        perror("socket");
+        freeaddrinfo(servinfo);
+        return 1;
+    }
 
-    
-    sendto(sockfd, "ftp", 3, 0,
-               (struct sockaddr *)&server_addr,
-               sizeof(server_addr));
-    
+    // Send "ftp" to server (this is the step your code was missing)
+    const char *msg = "ftp";
+    ssize_t sent = sendto(sockfd, msg, strlen(msg), 0, p->ai_addr, p->ai_addrlen);
+    if (sent < 0) {
+        perror("sendto");
+        close(sockfd);
+        freeaddrinfo(servinfo);
+        return 1;
+    }
+
+    // Receive reply ("yes" / "no")
     char buffer[BUF_SIZE];
-    socklen_t addr_len = sizeof(server_addr);
+    struct sockaddr_storage sender_addr;
+    socklen_t addr_len = sizeof sender_addr;
 
     ssize_t n = recvfrom(sockfd, buffer, BUF_SIZE - 1, 0,
-                         (struct sockaddr *)&server_addr,
-                         &addr_len);
+                         (struct sockaddr *)&sender_addr, &addr_len);
+    if (n < 0) {
+        perror("recvfrom");
+        close(sockfd);
+        freeaddrinfo(servinfo);
+        return 1;
+    }
+    buffer[n] = '\0';
 
-    buffer[n] = '\0'; 
     if (strcmp(buffer, "yes") == 0) {
         printf("A file transfer can start.\n");
+    } else {
+        // "no" or anything else => exit silently (per lab)
+        close(sockfd);
+        freeaddrinfo(servinfo);
+        return 1;
     }
 
     close(sockfd);
+    freeaddrinfo(servinfo);
     return 0;
-
 }
