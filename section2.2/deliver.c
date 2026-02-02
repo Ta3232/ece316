d3f95d2e7b11004bfec85143783fdca5e49b7c41
Ta3232 <84426477+Ta3232@users.noreply.github.com>
2026-02-02 16:19:30 -0500

    Update deliver.c

--- section2.2/deliver.c
+++ section2.2/deliver.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <time.h>
 
 #define DATA_SIZE 1000
 #define BUF_SIZE 4096
@@ -40,6 +41,8 @@ int main(int argc, char *argv[]) {
     char packet[BUF_SIZE];
     char ctrlbuf[128]; //ACK or NACK
     unsigned char data[DATA_SIZE];
+    struct timespec sendtime, recvtime; //time logic
+    double expected_rtt = 0.5;
 
     for (unsigned int frag_no = 1; frag_no <= total_frag; ) {
         size_t size = fread(data, 1, DATA_SIZE, fp); //read up to 1000Bytes 
@@ -47,7 +50,7 @@ int main(int argc, char *argv[]) {
         int header_len = snprintf(packet, sizeof(packet),
                                   "%u:%u:%zu:%s:", total_frag, frag_no, size, filename);
         memcpy(packet + header_len, data, size);
-
+        clock_gettime (CLOCK_MONOTONIC, &sendtime); //
         sendto(sockfd, packet, header_len + size, 0, res->ai_addr, res->ai_addrlen);
 
         while (1) {
@@ -56,6 +59,9 @@ int main(int argc, char *argv[]) {
 
             ssize_t rn = recvfrom(sockfd, ctrlbuf, sizeof(ctrlbuf)-1, 0, (struct sockaddr *)&server, &server_len);
             ctrlbuf[rn] = '\0';
+            clock_gettime(CLOCK_MONOTONIC, &recvtime);
+            double rtt = (recvtime.tv_sec - sendtime.tv_sec) + (recvtime.tv_nsec - sendtime.tv_nsec)/1e9;
+            printf("fragment %d sent RTT:%f s\n", frag_no, rtt);
 
             char type[8];
             unsigned int fno;
@@ -63,11 +69,12 @@ int main(int argc, char *argv[]) {
             if (sscanf(ctrlbuf, "%7[^:]:%u", type, &fno) != 2) continue;
             if (fno != frag_no) continue;
 
-            if (strcmp(type, "ACK") == 0) {
+            if (strcmp(type, "ACK") == 0 && rtt <= expected_rtt) {
                 frag_no++;   // move to next fragment
                 break;
             }
-            if (strcmp(type, "NACK") == 0) {
+                    
+            if (strcmp(type, "NACK") == 0 || strcmp(type, "ACK") == 0 && rtt > expected_rtt) {
                 // resend same fragment
                 sendto(sockfd, packet, header_len + size, 0, res->ai_addr, res->ai_addrlen);
             }
